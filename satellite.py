1e78ee844f03db352a939fb81f3e4e932da555e7
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2022-01-07 14:26:36 +0100

    Update satellite.py

--- satellite.py
+++ satellite.py
@@ -10,6 +10,8 @@ import skimage.measure
 import numpy as np
 import numpy as np
 import cv2
+from skan.csr import make_degree_image
+
 import matplotlib.pyplot as plt
 
 class road_map:
@@ -25,11 +27,14 @@ class road_map:
 		upper_yellow = np.array([178, 249, 260])
 		yellow_range = cv2.inRange(self.image, lower_yellow,upper_yellow)
 		self.image[yellow_range!=0] = [255,255,255]
-		self.image[np.where((im!=[255,255,255]).all(axis=2))] = [0,0,0]
+		self.image[np.where((self.image!=[255,255,255]).all(axis=2))] = [0,0,0]
 
-		self.image= cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+		self.image= cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 		self.image[np.where((self.image!=[255]))] = [0]
-		def nearest_nonzero_idx_v2(self,a,x,y):
+		kernel = np.int8([[-1, -1, -1],[-1, +1, -1],[-1, -1, -1]])
+		neighbors_all_zero = cv2.morphologyEx(src=self.image, op=cv2.MORPH_HITMISS, kernel=kernel)
+		self.image = self.image & ~neighbors_all_zero
+	def nearest_nonzero_idx_v2(self,a,x,y):
 		a.astype(int)
 		nonzero = cv2.findNonZero(a)
 		
@@ -48,16 +53,16 @@ class road_map:
 		roadwidth=[]
 		
 		
-		self._img = np.uint8(self._img)
-		binary = self._img > filters.threshold_otsu(self._img)
+		self.image = np.uint8(self.image)
+		binary = self.image > filters.threshold_otsu(self.image)
 		
 		np.unique(binary)
 		skel, distance = medial_axis(binary, return_distance=True)
 		dist_on_skel = distance * skel
 	
-		cv2.imwrite("skelssat"+str(self._tel)+".png",heatmap)
+		#cv2.imwrite("skelssat"+str(self._tel)+".png",heatmap)
 		#find crossings 
-		_, _, degrees = skeleton_to_csgraph(skel)
+		degrees = make_degree_image(skel)
 		intersection_matrix = degrees > 2
 		
 		#list with crossings
@@ -70,46 +75,6 @@ class road_map:
 				if int(l)!=0:
 		
 					dist_on_skel[teller][teller2]=255
-		for k in lists:
-			dist_on_skel[k[0]][k[1]]=0
-		retval, labels = cv2.connectedComponents(np.uint8(dist_on_skel))
-		list_of_labels=[]
-		list_of_lanes=[]
-		indexes=[]
-		
-		for k in self._points[1:]:
-			
-			l=self.nearest_nonzero_idx_v2(labels,k[0],k[1])
-			
-			label=labels[l[0][1]][l[0][0]]
-				
-			try:
-				index=list_of_labels.index(label)
-				indexes.append(len(roadwidth))
-			except:
-				print("except")
-				indexes.append(len(roadwidth))
-				sumtotal=0
-				number=0
-				list_of_labels.append(label)
-				for teller1,k in enumerate(labels):
-					for teller2,l in enumerate(k):
-						
-						if l==label:
-							if logger[teller1][teller2]>2:
-								"""
-								sumtotal+=logger[teller1][teller2]
-								number+=1
-			if sumtotal>0:
-				list_of_lanes.append(sumtotal/number)
-				list_of_labels.append(label)
-		
-			
-								roadwidth.append(sumtotal/number)
-								"""
-								roadwidth.append(logger[teller1][teller2])
-								print(logger[teller1][teller2])
-				
-		
-		return after,roadwidth,indexes
+    
+		return dist_on_skel
 
